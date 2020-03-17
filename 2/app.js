f6785d16e94926fddf2ad24fe4cdf0c6f9c09ee7
小熊逹哉 <tatsuya@ogumatatsuyanoMacBook-Pro.local>
2020-03-17 16:47:40 +0900

    [finish]6-1,6-2

---
+++ 2/app.js
@@ -0,0 +1,30 @@
+var hello = 'Hello World';
+alert(hello);
+
+//整数を代入する
+var int1 = 1;
+
+//負数を代入する
+var int2 = -10;
+
+//小数点を代入する
+var float1 = 3.14;
+
+//文字列を代入する
+var str1 = 'JavaScriptを覚えよう';
+
+alert(str1);
+
+//足し算
+alert(4+3);
+
+//引き算
+alert(8-5);
+
+//掛け算
+alert(2*6);
+
+//割り算
+alert(10/2);
+
+alert('Hello'+'World!!');
---
+++ 2/app5.js
@@ -0,0 +1,7 @@
+alert(3+4);
+
+alert(5-5);
+
+alert(6*0);
+
+alert(9/3);
